b4091bc6d0e4f3db8ff0a9a1f5314702ed3a2d0d
Extazy1 <2056345020@qq.com>
2024-05-07 19:30:49 +0800

    修改UserController中的数据返回格式

--- src/main/java/com/controller/UserController.java
+++ src/main/java/com/controller/UserController.java
@@ -22,7 +22,7 @@ public class UserController {
     private UserService userService;
 
     @ApiOperation(value = "用户注册", notes = "用户进行注册")
-    @PostMapping("register")
+    @PostMapping("/register")
     public Map<String, Object> register(
             @ApiParam(value = "用户名", required = true) @RequestParam String userName,
             @ApiParam(value = "密码", required = true) @RequestParam String password,
@@ -30,17 +30,29 @@ public class UserController {
             @ApiParam(value = "地址", required = true) @RequestParam String address,
             HttpServletRequest request) {
         Map<String, Object> result = new HashMap<>();
+        Map<String, Object> data = new HashMap<>();
         String IP = IpUtil.getIpAddr(request);
         int flag = userService.insertUser(phoneNumber, password, userName, address, IP);
-        if (flag == 0) {
-            result.put("msg", "用户名或手机号已存在");
-            result.put("code", 0);
-        } else if (flag == 1) {
-            result.put("msg", "IP限制注册");
-            result.put("code", 0);
-        } else if (flag == 2) {
-            result.put("msg", "注册成功");
-            result.put("code", 1);
+        switch (flag) {
+            case 0:
+                result.put("msg", "用户名或手机号已存在");
+                result.put("code", 0);
+                result.put("data", data);
+                break;
+            case 1:
+                result.put("msg", "IP限制注册");
+                result.put("code", 0);
+                result.put("data", data);
+                break;
+            case 2:
+                result.put("msg", "注册成功");
+                result.put("code", 1);
+                data.put("userName", userName);
+                data.put("phoneNumber", phoneNumber);
+                data.put("address", address);
+                data.put("ip", IP);
+                result.put("data", data);
+                break;
         }
         return result;
     }
@@ -54,22 +66,30 @@ public class UserController {
         User user = userService.selectUserByUserName(userName, password);
         int flag = userService.selectUserByUserName1(userName, password);
         Map<String, Object> result = new HashMap<>();
-        if (flag == 0) {
-            HttpSession session = request.getSession();
-            session.setAttribute("user", user);
-            result.put("code", "1");
-            result.put("msg", "登录成功");
-            result.put("id", user.getId());
-            result.put("name", user.getUserName());
-            result.put("phone", user.getPhoneNumber());
-            result.put("address", user.getAddress());
-            result.put("ip", IpUtil.getIpAddr(request));
-        } else if (flag == 1) {
-            result.put("code", "0");
-            result.put("msg", "用户未注册");
-        } else {
-            result.put("code", "0");
-            result.put("msg", "密码错误");
+        Map<String, Object> data = new HashMap<>();
+        switch (flag) {
+            case 0:
+                HttpSession session = request.getSession();
+                session.setAttribute("user", user);
+                result.put("code", 0);
+                result.put("msg", "登录成功");
+                data.put("id", user.getId());
+                data.put("name", user.getUserName());
+                data.put("phone", user.getPhoneNumber());
+                data.put("address", user.getAddress());
+                data.put("ip", IpUtil.getIpAddr(request));
+                result.put("data", data);
+                break;
+            case 1:
+                result.put("code", 0);
+                result.put("msg", "用户未注册");
+                result.put("data", data);
+                break;
+            default:
+                result.put("code", 0);
+                result.put("msg", "密码错误");
+                result.put("data", data);
+                break;
         }
         return result;
     }
